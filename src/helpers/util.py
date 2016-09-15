211558e04e35a247ab0b557ef5de5bd56e76ac92
Abhishek <abhicoders@gmail.com>
2016-09-15 17:42:35 +0530

    Fix the parsing function.

--- src/helpers/util.py
+++ src/helpers/util.py
@@ -1,4 +1,18 @@
+import json
+
 from sklearn.preprocessing import LabelEncoder
+from sklearn.base import BaseEstimator, TransformerMixin
+
+from nltk.stem import WordNetLemmatizer
+from nltk.stem.snowball import SnowballStemmer
+from nltk.stem.porter import PorterStemmer
+from nltk import word_tokenize
+
+
+
+sns = SnowballStemmer(language='english')
+por = PorterStemmer()
+
 
 def encode_variable(train, test):
     """
@@ -48,3 +62,48 @@ def load(filename):
         infile.close()
         
         return data
+
+
+class LemmaTokenizer(object):
+    def __init__(self):
+        self.wnl = WordNetLemmatizer()
+    
+    def __call__(self, doc):
+        return [self.wnl.lemmatize(t) for t in word_tokenize(doc)]
+
+class StemTokenizer(object):
+    def __init__(self):
+        self.sns = sns
+    
+    def __call__(self, doc):
+        return [self.sns.stem(t) for t in word_tokenize(doc)]
+
+
+class VarSelect(BaseEstimator, TransformerMixin):
+    def __init__(self, keys):
+        self.keys = keys
+        
+    def fit(self, X, y=None):
+        return self
+    
+    def transform(self, df):
+        return df[self.keys]
+
+def remove_non_alphanumeric(df):
+    return df.replace(r'[^A-Za-z0-9]+', ' ', regex=True)
+
+
+def convert_to_json(boilerplate):
+    return list(map(json.loads, boilerplate))
+
+def get_component(boilerplate, key):
+    """
+    Get value for a particular key in boilerplate json,
+    if present return the value else return an empty string
+    
+    boilerplate: list of boilerplate text in json format
+    key: key for which we want to fetch value e.g. body, title and url
+    """
+    
+    return np.array([bp[key] if key in bp and bp[key] else u'' for bp in boilerplate])  
+
